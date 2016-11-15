092445a621d5a92266f836db47b39e5606172acf
Jen Kagan <kaganjd@gmail.com>
2016-11-14 21:18:35 -0500

    add luminance

--- app.js
+++ app.js
@@ -5,31 +5,18 @@ var lightBulb = new Object();
 
 app.use(express.static('public'));
 
-app.post('/color/r/:rColor/g/:gColor/b/:bColor', function(req, res){
+app.post('/color/r/:rColor/g/:gColor/b/:bColor/l/:luminance', function(req, res){
   var data = req.params;
   lightBulb.redColor = data.rColor;
   lightBulb.greenColor = data.gColor;
   lightBulb.blueColor = data.bColor;
-  res.send('red: ' + data.rColor + ' green: ' + data.gColor + ' blue: ' + data.bColor);
+  lightBulb.luminance = data.luminance;
+  res.send('red: ' + data.rColor + ' green: ' + data.gColor + ' blue: ' + data.bColor + ' luminance: ' + data.luminance);
 });
 
 app.get('/color', function(req, res){
   res.send(lightBulb);
 });
-
-/*
-app.route('/book/:title')
-  .get(function (req, res) {
-    var data = req.params;
-    res.send('Get book ' + data.title);
-  })
-  .post(function (req, res) {
-    res.send('Add a book');
-  })
-  .put(function (req, res) {
-    res.send('Update the book');
-  })
-*/
 
 app.listen(9999, function () {
   console.log('Example app listening on port 9999!');
